9aee209ba7634012eebc6c935514ce1eb3c901e9
Tim Caswell <tim@creationix.com>
2012-11-03 15:33:36 -0500

    Update iconbutton.js
    Add .enableIf

--- iconbutton.js
+++ iconbutton.js
@@ -3,7 +3,11 @@ function IconButton(icon, title, onClick) {
   this.el = document.createElement("a");
   this.el.className = "iconbutton";
   this.el.setAttribute("title", title);
-  this.el.addEventListener("click", onClick, false);
+  var self = this;
+  this.el.addEventListener("click", function (evt) {
+    if (this.disabled) return;
+    return onClick(evt);
+  }, false);
   var iconEl = document.createElement("i");
   iconEl.className = icon;
   this.el.appendChild(iconEl);
@@ -13,3 +17,13 @@ IconButton.prototype.resize = function (width, height) {
   this.el.style.width = (width - 6) + "px";
   this.el.style.height = (height - 2) + "px";
 };
+
+IconButton.prototype.enableIf = function (condition) {
+  this.disabled = !condition;
+  if (condition) {
+    this.el.classList.remove("disabled");
+  }
+  else {
+    this.el.classList.add("disabled");
+  }
+}
